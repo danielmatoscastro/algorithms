0414d2c4e0eecef5462f125e6d66e1f25c320c23
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-01-03 22:30:32 -0300

    adição de javadoc para Exercicio1113

--- src/br/com/algorithms/capitulo1/Exercicio1113.java
+++ src/br/com/algorithms/capitulo1/Exercicio1113.java
@@ -2,8 +2,17 @@ package br.com.algorithms.capitulo1;
 
 import edu.princeton.cs.introcs.StdOut;
 
+/**
+ * Classe com métodos para a resolução do exercício 1.1.13
+ *
+ */
 public class Exercicio1113 {
 	
+	/**
+	 * Dada uma matriz, imprime sua transposta na tela.
+	 * 
+	 * @param matriz genérica cuja transposta será impressa.
+	 * */
 	public static <T> void printTransposta(T[][] matriz){
 		for (int i = 0; i < matriz[0].length; i++) {
 			for (int j = 0; j < matriz.length; j++) {
